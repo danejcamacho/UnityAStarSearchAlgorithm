8bb94c8da51d39bb6be2c006caf3bbfbe85f3b77
Jhoshua Ampo <ampojhoshua@gmail.com>
2024-11-20 15:19:01 -0800

    update player controls

--- AStarSearch/Assets/Scripts/Player/InputSystem_Actions.cs
+++ AStarSearch/Assets/Scripts/Player/InputSystem_Actions.cs
@@ -107,6 +107,15 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Break"",
+                    ""type"": ""Button"",
+                    ""id"": ""ab3782d3-35fc-4412-95da-d69c78c74393"",
+                    ""expectedControlType"": """",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": false
                 }
             ],
             ""bindings"": [
@@ -492,6 +501,17 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""groups"": ""Keyboard&Mouse"",
                     ""action"": ""Crouch"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""6f29ab29-fe33-4baf-88f1-b87047a82992"",
+                    ""path"": ""<Keyboard>/space"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Break"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
                 }
@@ -1088,6 +1108,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         m_Player_Previous = m_Player.FindAction("Previous", throwIfNotFound: true);
         m_Player_Next = m_Player.FindAction("Next", throwIfNotFound: true);
         m_Player_Sprint = m_Player.FindAction("Sprint", throwIfNotFound: true);
+        m_Player_Break = m_Player.FindAction("Break", throwIfNotFound: true);
         // UI
         m_UI = asset.FindActionMap("UI", throwIfNotFound: true);
         m_UI_Navigate = m_UI.FindAction("Navigate", throwIfNotFound: true);
@@ -1176,6 +1197,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
     private readonly InputAction m_Player_Previous;
     private readonly InputAction m_Player_Next;
     private readonly InputAction m_Player_Sprint;
+    private readonly InputAction m_Player_Break;
     public struct PlayerActions
     {
         private @InputSystem_Actions m_Wrapper;
@@ -1189,6 +1211,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         public InputAction @Previous => m_Wrapper.m_Player_Previous;
         public InputAction @Next => m_Wrapper.m_Player_Next;
         public InputAction @Sprint => m_Wrapper.m_Player_Sprint;
+        public InputAction @Break => m_Wrapper.m_Player_Break;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -1225,6 +1248,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Sprint.started += instance.OnSprint;
             @Sprint.performed += instance.OnSprint;
             @Sprint.canceled += instance.OnSprint;
+            @Break.started += instance.OnBreak;
+            @Break.performed += instance.OnBreak;
+            @Break.canceled += instance.OnBreak;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -1256,6 +1282,9 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
             @Sprint.started -= instance.OnSprint;
             @Sprint.performed -= instance.OnSprint;
             @Sprint.canceled -= instance.OnSprint;
+            @Break.started -= instance.OnBreak;
+            @Break.performed -= instance.OnBreak;
+            @Break.canceled -= instance.OnBreak;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -1447,6 +1476,7 @@ public partial class @InputSystem_Actions: IInputActionCollection2, IDisposable
         void OnPrevious(InputAction.CallbackContext context);
         void OnNext(InputAction.CallbackContext context);
         void OnSprint(InputAction.CallbackContext context);
+        void OnBreak(InputAction.CallbackContext context);
     }
     public interface IUIActions
     {
--- AStarSearch/Assets/Scripts/Player/PlayerControls.cs
+++ AStarSearch/Assets/Scripts/Player/PlayerControls.cs
@@ -1,17 +1,31 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
 
 public class PlayerControls : MonoBehaviour
 {
     [SerializeField]
-    private float moveSpeed = 0;
+    private float acceleration = 500f;
+    [SerializeField]
+    private float breakingForce = 300f;
+    [SerializeField]
+    private float maxTurnAngle = 15f;
+
+    private float currentAcceleration = 0f;
+    private float currentBrakingForce = 0f;
+    private float currentTurnAngle = 0f;
+    
+
     InputSystem_Actions inputActions;
     Rigidbody rb;
     Vector2 moveInput;
 
+    List<WheelCollider> wheels = new();
+
     private void Awake() {
         inputActions = new();
         rb = GetComponent<Rigidbody>();    
+        GetComponentsInChildren<WheelCollider>(wheels);
     }
     
     private void OnEnable() {
@@ -21,23 +35,30 @@ public class PlayerControls : MonoBehaviour
     private void OnDisable() {
         inputActions.Disable();
     }
-    void Start()
-    {
-        
-    }
 
-    private void Update() {
+    private void FixedUpdate() {
         moveInput = inputActions.Player.Move.ReadValue<Vector2>();
-        Move();
-    }
+        currentTurnAngle = maxTurnAngle * moveInput.x;
+        currentAcceleration = acceleration * moveInput.y;
+        currentBrakingForce = (inputActions.Player.Break.ReadValue<float>() > 0f) ? currentBrakingForce = breakingForce : 0f;
 
-    private void Move() {
-        Vector3 moveForce = transform.forward * moveInput.y;
-        moveForce *= moveSpeed;
-        rb.AddForce(moveForce);
-    }
+        // accelerate
+        // wheels[0].motorTorque = currentAcceleration;
+        // wheels[1].motorTorque = currentAcceleration;
+        wheels[2].motorTorque = currentAcceleration;
+        wheels[3].motorTorque = currentAcceleration;
 
-    private void Turn() {
-        
+        // break
+        wheels[0].brakeTorque = currentBrakingForce;
+        wheels[1].brakeTorque = currentBrakingForce;
+        wheels[2].brakeTorque = currentBrakingForce;
+        wheels[3].brakeTorque = currentBrakingForce;
+
+        // turn
+        wheels[0].steerAngle = currentTurnAngle;
+        wheels[1].steerAngle = currentTurnAngle;
+
+        wheels[0].transform.localRotation = Quaternion.Euler(0,currentTurnAngle, 0);
+        wheels[1].transform.localRotation = Quaternion.Euler(0,currentTurnAngle, 0);
     }
 }
